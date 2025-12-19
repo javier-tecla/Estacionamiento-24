4a1a7b73ac5edd5d82d63cc534633bb31f01f3ae
Javier Borjas <cristman11@gmail.com>
2025-12-19 00:52:46 -0300

    Agregando DataTables

--- resources/views/admin/roles/index.blade.php
+++ resources/views/admin/roles/index.blade.php
@@ -33,12 +33,12 @@
                 <div class="card-body">
                     <div class="row">
                         <div class="col-md-12">
-                            <div table-responsive>
-                                <table class="table table-bordered table-striped table-hover table-sm">
+                            <div class="table-responsive">
+                                <table id="table1" class="table table-bordered table-striped table-hover table-sm">
                                     <thead>
                                         <tr>
                                             <th style="width: 10px">Nro</th>
-                                            <th>Rol</th>
+                                            <th>Nombre del Rol</th>
                                             <th>Acciones</th>
                                         </tr>
                                     </thead>
@@ -47,9 +47,10 @@
                                             <tr>
                                                 <td style="text-align: center">{{ $loop->iteration }}</td>
                                                 <td>{{ $role->name }}</td>
-                                                <td>
-                                                    <a href="#" class="btn btn-xs btn-info">Editar</a>
-                                                    <a href="#" class="btn btn-xs btn-danger">Eliminar</a>
+                                                <td class="d-flex justify-content-center">
+                                                    <a href="#" class="btn btn-xs btn-warning mx-1"><i class="fas fa-check"></i> Asignar permisos</a>
+                                                    <a href="#" class="btn btn-xs btn-success mx-1"><i class="fas fa-edit"></i> Editar</a>
+                                                    <a href="#" class="btn btn-xs btn-danger mx-1"><i class="fas fa-trash-alt"></i> Eliminar</a>
                                                 </td>
                                             </tr>
                                         @endforeach
@@ -67,12 +68,115 @@
 @stop
 
 @section('css')
-    {{-- Add here extra stylesheets --}}
-    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+    <style>
+        /* Fondo transparente y sin borde en el contenedor */
+        #table1_wrapper .dt-buttons {
+            background-color: transparent;
+            box-shadow: none;
+            border: none;
+            display: flex;
+            justify-content: center;
+            /* Centrar los botones */
+            gap: 10px;
+            /* Espaciado entre botones */
+            margin-bottom: 15px;
+            /* Separar botones de la tabla */
+        }
+
+        /* Estilo personalizado para los botones */
+        #table1_wrapper .btn {
+            color: #fff;
+            /* Color del texto en blanco */
+            border-radius: 4px;
+            /* Bordes redondeados */
+            padding: 5px 15px;
+            /* Espaciado interno */
+            font-size: 14px;
+            /* Tamaño de fuente */
+        }
+
+        /* Colores por tipo de botón */
+        .btn-danger {
+            background-color: #dc3545;
+            border: none;
+        }
+
+        .btn-success {
+            background-color: #28a745;
+            border: none;
+        }
+
+        .btn-info {
+            background-color: #17a2b8;
+            border: none;
+        }
+
+        .btn-warning {
+            background-color: #ffc107;
+            color: #212529;
+            border: none;
+        }
+
+        .btn-default {
+            background-color: #6e7176;
+            color: #212529;
+            border: none;
+        }
+    </style>
 @stop
 
 @section('js')
     <script>
-        console.log("Hi, I'm using the Laravel-AdminLTE package!");
+        $(function() {
+            $("#table1").DataTable({
+                "pageLength": 10,
+                "language": {
+                    "emptyTable": "No hay información",
+                    "info": "Mostrando _START_ a _END_ de _TOTAL_ Roles",
+                    "infoEmpty": "Mostrando 0 a 0 de 0 Roles",
+                    "infoFiltered": "(Filtrado de _MAX_ total Roles)",
+                    "lengthMenu": "Mostrar _MENU_ Roles",
+                    "loadingRecords": "Cargando...",
+                    "processing": "Procesando...",
+                    "search": "Buscador:",
+                    "zeroRecords": "Sin resultados encontrados",
+                    "paginate": {
+                        "first": "Primero",
+                        "last": "Último",
+                        "next": "Siguiente",
+                        "previous": "Anterior"
+                    }
+                },
+                "responsive": true,
+                "lengthChange": true,
+                "autoWidth": false,
+                buttons: [{
+                        text: '<i class="fas fa-copy"></i> COPIAR',
+                        extend: 'copy',
+                        className: 'btn btn-default'
+                    },
+                    {
+                        text: '<i class="fas fa-file-pdf"></i> PDF',
+                        extend: 'pdf',
+                        className: 'btn btn-danger'
+                    },
+                    {
+                        text: '<i class="fas fa-file-csv"></i> CSV',
+                        extend: 'csv',
+                        className: 'btn btn-info'
+                    },
+                    {
+                        text: '<i class="fas fa-file-excel"></i> EXCEL',
+                        extend: 'excel',
+                        className: 'btn btn-success'
+                    },
+                    {
+                        text: '<i class="fas fa-print"></i> IMPRIMIR',
+                        extend: 'print',
+                        className: 'btn btn-warning'
+                    }
+                ]
+            }).buttons().container().appendTo('#table1_wrapper .row:eq(0)');
+        });
     </script>
 @stop
