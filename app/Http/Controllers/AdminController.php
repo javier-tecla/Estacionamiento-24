bd988b5f63fa1a82b1f09b50c3ca2d17b6c9c01a
Javier Borjas <cristman11@gmail.com>
2026-01-24 01:22:08 -0300

    Mejorando el Dashboard con Cards

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
 
 class AdminController extends Controller
 {
     public function index()
     {
-        return view('admin.index');
+        $total_roles = Role::count();
+        return view('admin.index', compact('total_roles'));
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,18 +1,50 @@
 @extends('adminlte::page')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1><b>Bienvenido: </b>{{ Auth::user()->name }}</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href=""><b>Rol:
+                                {{ Auth::user()->roles->pluck('name')->implode(',') }}</b></a> </li>
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+
+    <hr>
+
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
-@stop
+    <div class="container-fluid">
+        <div class="row">
 
-@section('css')
-    {{-- Add here extra stylesheets --}}
-    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
-@stop
+            <div class="col-md-3 col-sm-6 col-12">
+                <div class="info-box">
+                    <span class="info-box-icon bg-info">
+                        <a href="{{ url('/admin/roles') }}">
+                            <img src="{{ url('/images/user.gif') }}" width="100%" alt="">
+                        </a>
+                    </span>
 
-@section('js')
-    <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
-@stop
+                    <div class="info-box-content">
+                        <span class="info-box-text">Roles registrados</span>
+                        <span class="info-box-number">{{ $total_roles }} roles</span>
+                    </div>
+                </div>
+            </div>
+        @stop
+
+        @section('css')
+            {{-- Add here extra stylesheets --}}
+            {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+        @stop
+
+        @section('js')
+            <script>
+                console.log("Hi, I'm using the Laravel-AdminLTE package!");
+            </script>
+        @stop
